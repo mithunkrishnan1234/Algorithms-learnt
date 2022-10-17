04fcc1cab8408ea9532fb012fa5586d5cece0a9b
mithunkrishnan1234 <115772513+mithunkrishnan1234@users.noreply.github.com>
2022-10-17 16:59:26 +0530

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,60 @@
 
-public class Main {
+import java.io.*;
 
+public class Main{
 
-    public static int binarySearch(int arr[], int item,int beg,int end) {
-        while (beg <= end) {
-            int midIndex = (beg + end ) / 2;
-            if (arr[midIndex] == item) {
-                return midIndex;
-            }
-            if (item<arr[midIndex]) {
-                end = midIndex - 1;
-            } else {
-                beg = midIndex + 1;
+    static void swap(int[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static int partition(int[] arr, int low, int high)
+    {
+
+        int pivot = arr[high];
+        int i = (low - 1);
+
+        for(int j = low; j <= high - 1; j++)
+        {
+
+            if (arr[j] < pivot)
+            {
+                i++;
+                swap(arr, i, j);
             }
         }
-        return -1;
+        swap(arr, i + 1, high);
+        return (i + 1);
     }
 
-
-    public static void main(String[] args) {
-
-       int[] arr = {10,11,16,20,23};
-      int res= binarySearch(arr,23,0,arr.length-1);
-
-          System.out.println(res);
-
-
+    static void quickSort(int[] arr, int low, int high)
+    {
+        if (low < high)
+        {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
     }
 
+    static void printArray(int[] arr, int size)
+    {
+        for(int i = 0; i < size; i++)
+            System.out.print(arr[i] + " ");
+
+        System.out.println();
+    }
+
+    public static void main(String[] args)
+    {
+        int[] arr = { 10, 7, 8, 9, 1, 5 };
+        int n = arr.length;
+
+        quickSort(arr, 0, n - 1);
+        System.out.println("Sorted array: ");
+        printArray(arr, n);
+    }
 }
+
